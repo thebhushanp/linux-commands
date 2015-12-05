9024551e492c1f5436370dee600ad544e2637c28
Bhushan Patil <bhushanngage@gmail.com>
2015-12-05 22:33:39 +0530

    Added few general commands

---
+++ general.sh
@@ -0,0 +1,4 @@
+ls -lh filename  *displays size of file* 
+tar xf tar filename  *extract tar file*
+// Find the biggest directories, so to get a sorted list containing the 20 biggest dirs.
+du -m /some/path | sort -nr | head -n 20
