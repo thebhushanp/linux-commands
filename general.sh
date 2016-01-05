19b7d8e903239e5ac8f4b40d3af2782b7d143c3b
Bhushan Patil <bhushanngage@gmail.com>
2016-01-05 11:12:22 +0530

    Update general.sh

--- general.sh
+++ general.sh
@@ -1,4 +1,11 @@
-ls -lh filename  *displays size of file* 
-tar xf tar filename  *extract tar file*
+// Displays size of file
+ls -lh filename
+
+// Extract tar file
+tar xf tar filename
+
 // Find the biggest directories, so to get a sorted list containing the 20 biggest dirs.
 du -m /some/path | sort -nr | head -n 20
+
+// cURL with proxy
+curl -x http://10.1.1.253:3128 --proxy-user username:password -L http://www.google.com
