64f48ec65d0ff4c02905b13f8f884c7767e12720
Bhushan Patil <bhushanngage@gmail.com>
2016-01-21 18:18:40 +0530

    Update general.sh

--- general.sh
+++ general.sh
@@ -23,3 +23,9 @@ ls -lh file_name
 git config --global http.proxy http://proxyuser:proxypwd@proxy.server.com:8080
 git config --global https.proxy https://proxyuser:proxypwd@proxy.server.com:8080
 
+// Pass argument to shell file
+  $*  Returns a single string (``$1, $2 ... $n'') comprising all of the positional parameters separated by the internal field separator character
+  $@  Returns a sequence of strings (``$1'', ``$2'', ... ``$n'') wherein each positional parameter remains separate from the others. 
+  $1, $2 ... $n   Refers to a numbered argument to the script
+  $0  Refers to the name of the script itself.
+  $#  Refers to the number of arguments specified on a command line. 
